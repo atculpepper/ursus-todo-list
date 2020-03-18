26f88d7901e29ead6222de64b6b3fa31ab1fc059
Anne Culpepper <anne.culpepper@gmail.com>
2020-03-18 18:40:31 -0500

    click event captured and logged to console

--- client.js
+++ client.js
@@ -1,10 +1,38 @@
 $(document).ready(init);
 
+let toDo = [];
 function init() {
   console.log("Looks like we are connected :)");
-  $(".js-addTask-btn").on("click", addNewTask);
+  $(".js-addTask-btn").on("click", clickAddNewTask);
 }
 
-function addNewTask() {
+function clickAddNewTask(event) {
+  //prevent automatic reload page
+  event.preventDefault();
   console.log("You clicked to submit your task");
+  const taskName = $(".js-TODO-input").val();
+  const completed = false;
+
+  console.log("You have to" + " " + taskName + " " + "NOW!");
+
+  $(".js-TODO-input").val("");
+  //call to the function that will push these values into the toDo array
+  addNewTask();
 }
+
+// function addNewTask() {
+//   const newTask = {
+//     name: taskInput,
+//     completed: false
+//   };
+//   toDo.push(newTask);
+//   render();
+// }
+
+// function render(){
+//   for (let aTask of toDo){
+//     $(".js-container").append (`
+//     <li>
+//     `)
+//   }
+// }
